37244868fde78893d0a02eb75f1e48d5496d9eb4
Justin Plock <justin@plock.net>
2026-02-09 09:02:47 -0500

    major version bumps

--- web/rails-omniauth/config/application.rb
+++ web/rails-omniauth/config/application.rb
@@ -3,7 +3,7 @@ require 'rails/all'
 
 module VouchExample
   class Application < Rails::Application
-    config.load_defaults 7.1
+    config.load_defaults 8.0
     config.secret_key_base = ENV.fetch('SECRET_KEY_BASE') { SecureRandom.hex(64) }
   end
 end
